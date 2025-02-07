d59a444b8b2ccb3bcc59b569b5b53403a2f23c0d
Brooks Lybrand <brookslybrand@gmail.com>
2025-02-07 17:01:49 -0600

    Remove some dependencies

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -9,7 +9,7 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export function loader({ context }: Route.LoaderArgs) {
-  return { message: "Hello from Vercel" };
+  return { message: "Bla!" };
 }
 
 export default function Home({ loaderData }: Route.ComponentProps) {
